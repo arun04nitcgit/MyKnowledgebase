88ef2e6a8ec3d7bded97c7d3e310e44644dfcf65
arun04nitcgit <arun04nitc@outlook.com>
2018-06-01 23:50:00 -0400

    Update ReNameCol.R

--- ReNameCol.R
+++ ReNameCol.R
@@ -1,1 +1,26 @@
 # This is to get all the function to rename a Col for data frame
+
+colnames(newprice)<- c("premium","change","newprice")
+colnames(X)[2] <- "superduper"
+colnames(dataframe)[which(names(dataframe) == "columnName")] <- "newColumnName"
+
+
+names(newprice)[1]<-paste("premium")
+names(newprice)[1]<-"premium"
+names(newprice) <- c("premium","change","newprice")
+
+
+cols <- c("premium","change","newprice")
+colnames(dataframe) <- cols
+colnames(df)[which(colnames(df) %in% c("bad","worst") )] <- c("good","best")
+
+rename.columns=function(df,changelist){
+  #renames columns of a dataframe
+  for(i in 1:length(names(df))){
+    if(length(changelist[[names(df)[i]]])>0){
+      names(df)[i]= changelist[[names(df)[i]]]
+    }
+  }
+  df
+}
+
